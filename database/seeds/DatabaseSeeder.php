e55f2464dbf7459e6672aaabc6fcc214051b2919
haruocode <takaron1979@yahoo.co.jp>
2020-08-16 13:46:00 +0900

    seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call([
+            CategorySeeder::class,
+            TitleSeeder::class,
+            QuizSeeder::class
+        ]);
     }
 }
